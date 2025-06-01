e5b475425e67c8a209d6cc7b84b279a33b165c5e
palomino79 <palomino79@protonmail.com>
2025-06-01 13:51:29 -0400

    changed notification strategy

--- notify/vars.py
+++ notify/vars.py
@@ -3,6 +3,6 @@ from pytz import timezone  # type: ignore
 
 TIMEZONE = timezone(os.environ.get("TZ", "US/Eastern"))
 SCHEDULE_PATH = os.environ.get("SCHEDULE_PATH")
-PUSH_SERVICE_URL = os.environ.get("SERVICE")
+PUSH_SERVICE_URL = os.environ.get("PUSH_SERVICE_URL")
 TOPIC = os.environ.get("TOPIC")
 NOTIFICATION_URL = os.path.join(PUSH_SERVICE_URL or "", TOPIC or "")
